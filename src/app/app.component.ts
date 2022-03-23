79d2c12c94670e2b73d36e942fc48755943153c4
nathangiose <nathan@angleorange.com>
2022-03-23 20:28:25 +0200

    added functionality-nxt dlete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Task } from './events/task.component';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'TaskList';
+  tasks : Task[] = [];
+  newTask: string;
+
+  saveTask(){
+    if(this.newTask){
+      let task = new Task();
+      task.name = this.newTask;
+      task.isCompleted = true;
+      this.tasks.push(task);
+      this.newTask = "";
+    }else{
+      alert('Please enter a Task');
+    }
+  }
+
+  done(id:number){
+    this.tasks[id].isCompleted = !this.tasks[id].isCompleted;
+  }
 }
